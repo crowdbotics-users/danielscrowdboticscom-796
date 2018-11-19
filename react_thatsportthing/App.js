3be09a3f62585ffe0bed76dcaaf65d28e99b2669
Ishan Vyas <ishanvyasatc194@gmail.com>
2018-11-19 15:39:35 +0530

    update design

--- react_thatsportthing/App.js
+++ react_thatsportthing/App.js
@@ -48,7 +48,7 @@ const App = createStackNavigator(
     }
   },
   {
-    initialRouteName: "HomePage",
+    initialRouteName: "LoginType",
     headerMode: "float",
     navigationOptions: {
       gesturesEnabled: false,
