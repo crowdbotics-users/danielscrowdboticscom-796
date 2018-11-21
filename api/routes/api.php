3ce69ffb12d8aa2981b922b60adfaeea62f26ef6
Ishan Vyas <vyasishanatc194@gmail.com>
2018-11-21 16:02:10 +0530

    data

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,31 +18,31 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix' => 'users'], function () {
-    Route::post('register', 'Api\LoginController@register');
-    Route::post('login', 'Api\LoginController@login');
+    Route::post('register', 'API\LoginController@register');
+    Route::post('login', 'API\LoginController@login');
 
     Route::group(['middleware' => 'jwt.verify'], function () {
-        Route::get('logout', 'Api\LoginController@logout');
-        Route::get('edit_profile', 'Api\LoginController@edit_profile');
-        Route::post('post_profile', 'Api\LoginController@post_profile');
-        Route::post('upload_cover_profile', 'Api\UserController@cover_image');
-        Route::post('add_follower', 'Api\UserController@add_follower');
+        Route::get('logout', 'API\LoginController@logout');
+        Route::get('edit_profile', 'API\LoginController@edit_profile');
+        Route::post('post_profile', 'API\LoginController@post_profile');
+        Route::post('upload_cover_profile', 'API\UserController@cover_image');
+        Route::post('add_follower', 'API\UserController@add_follower');
 
         Route::group(['prefix' => 'gallery'], function () {
-            Route::get('list', 'Api\GalleryController@list_images');
-            Route::post('upload-images', 'Api\GalleryController@upload_images');
+            Route::get('list', 'API\GalleryController@list_images');
+            Route::post('upload-images', 'API\GalleryController@upload_images');
         });
 
         Route::group(['prefix' => 'posts'], function () {
-            Route::get('list', 'Api\PostController@list');
-            Route::post('add_post', 'Api\PostController@store');
-            Route::post('add_like', 'Api\PostController@add_like');
-            Route::post('add_comment', 'Api\PostController@add_comment');
-            Route::post('view_all_message', 'Api\PostController@view_all_message');
-            Route::post('view_reply_all_message', 'Api\PostController@view_reply_all_message');
+            Route::get('list', 'API\PostController@list');
+            Route::post('add_post', 'API\PostController@store');
+            Route::post('add_like', 'API\PostController@add_like');
+            Route::post('add_comment', 'API\PostController@add_comment');
+            Route::post('view_all_message', 'API\PostController@view_all_message');
+            Route::post('view_reply_all_message', 'API\PostController@view_reply_all_message');
         });
         Route::group(['prefix' => 'requests'], function () {
-            Route::post('send_request', 'Api\RequestController@send_request');
+            Route::post('send_request', 'API\RequestController@send_request');
         });   
 
     });
